df7a8feb347ad50b9b0ecef13de484f0f3814445
jhooc77 <jhooc77@gmail.com>
2023-03-03 18:16:02 +0900

    arclight support

--- Core/src/main/java/com/jhooc77/structureapi/StructureAPI.java
+++ Core/src/main/java/com/jhooc77/structureapi/StructureAPI.java
@@ -2,6 +2,8 @@ package com.jhooc77.structureapi;
 
 import org.bukkit.Bukkit;
 
+import java.util.logging.Logger;
+
 public final class StructureAPI {
 
     static String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
@@ -9,16 +11,33 @@ public final class StructureAPI {
 
     public static StructureManager getStructureManager() {
         int subVersion = Integer.parseInt(version.replace("v1_", "").replaceAll("_R\\d", ""));
+        Logger logger = Bukkit.getLogger();
+        try {
+            Class.forName("io.izzel.arclight.common.mod.server.ArclightServer");
+            switch (version) {
+                case "v1_19_R1":
+                    logger.info("StructureAPI loaded - v1_19_R1_Arclight");
+                    return new StructureManager_V1_19_R1_Arclight();
+                case "v1_19_R2":
+                    logger.info("StructureAPI loaded - v1_19_R2_Arclight");
+                    return new StructureManager_V1_19_R2_Arclight();
+            }
+        } catch (ClassNotFoundException e) {
+        }
         if (subVersion >= 17) {
+            logger.info("StructureAPI loaded - v1.17+");
             return new StructureManager_V1_17();
         } else {
             switch (version) {
                 case "v1_12_R1":
+                    logger.info("StructureAPI loaded - v1_12_R1");
                     return new StructureManager_V1_12_R1();
                 case "v1_16_R3":
-                    return new StructureManager_V1_16_R3();
+                    logger.info("StructureAPI loaded - v1_16_R3");
+                    return new StructureManager_V1_19_R2_Arclight();
             }
         }
+        logger.info("StructureAPI failed to load");
         return null;
     }
 
---
+++ V1_19_R2_Arclight/src/main/java/com/jhooc77/structureapi/Structure_V1_19_R2_Arclight.java
@@ -0,0 +1,36 @@
+package com.jhooc77.structureapi;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockRotProcessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.bukkit.Location;
+import org.bukkit.block.structure.Mirror;
+import org.bukkit.block.structure.StructureRotation;
+import org.bukkit.craftbukkit.v1_19_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_19_R2.util.RandomSourceWrapper;
+
+import java.util.Random;
+
+public class Structure_V1_19_R2_Arclight implements Structure {
+
+    final StructureTemplate structure;
+    public Structure_V1_19_R2_Arclight(StructureTemplate structure) {
+        this.structure = structure;
+    }
+    @Override
+    public void place(Location location, boolean spawnEntity, StructureRotation rotation, Mirror mirror, float integrity, Random random) {
+        ServerLevelAccessor worldAccess = ((CraftWorld) location.getWorld()).getHandle();
+        BlockPos blockPosition = new BlockPos(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        RandomSourceWrapper randomSource = new RandomSourceWrapper(random);
+        StructurePlaceSettings settings = new StructurePlaceSettings()
+                .m_74377_(net.minecraft.world.level.block.Mirror.valueOf(mirror.name()))
+                .m_74379_(Rotation.valueOf(rotation.name()))
+                .m_74392_(!spawnEntity)
+                .m_74383_(new BlockRotProcessor(1))
+                .m_230324_(randomSource);
+        structure.m_230328_(worldAccess, blockPosition, blockPosition, settings, randomSource, 2);
+    }
+}
